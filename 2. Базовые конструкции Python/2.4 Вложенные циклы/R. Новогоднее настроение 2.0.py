32be00bc0af1e31947f79148f528dbfb04b69dc5
ParfenovSergey <Parfenov.Sergey.mstu@mail.ru>
2025-04-19 15:26:51 +0300

    Update 2.4.R

--- 2. Базовые конструкции Python/2.4 Вложенные циклы/R. Новогоднее настроение 2.0.py
+++ 2. Базовые конструкции Python/2.4 Вложенные циклы/R. Новогоднее настроение 2.0.py
@@ -1,10 +1,15 @@
-n = int(input())
-i, length = 1, 1
-while i < n:
-    for j in range(1, length):
-        if i <= n:
-            print(i, end=' ')
-        i += 1
-    else:
-        print()
-        length += 1
+n, width = int(input()), 1
+for k in range(2):
+    i, length = 1, 1
+    while i <= n:
+        line = ''
+        for j in range(0, length):
+            if i <= n:
+                line += f'{i} '
+            i += 1
+        else:
+            if k == 0:
+                width = max(width, len(line))
+            else:
+                print(f'{line:^{width}}')
+            length += 1
